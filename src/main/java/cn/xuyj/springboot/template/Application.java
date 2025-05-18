30120798d672bdc7f4499cce442069a54310f73c
xuyj <1768335576@qq.com>
2025-05-18 21:01:24 +0800

    feat:基础框架的搭建

---
+++ src/main/java/cn/xuyj/springboot/template/Application.java
@@ -0,0 +1,17 @@
+package cn.xuyj.springboot.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: xuyj
+ * @Date: 2025/5/18 20:23
+ * @Email: 1768335576@qq.com
+ * @Desc：类描述
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
